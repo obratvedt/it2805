042bdf882bc0a6541c6f14ca29808e1c92de4a88
obratvedt <oyvindbratvedt@hotmail.com>
2014-11-20 12:59:08 +0100

    sg

--- webcode/movies/movies.js
+++ webcode/movies/movies.js
@@ -6,6 +6,7 @@ if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
 else {// code for IE6, IE5
   xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   }
+ xmlhttp.open("GET","movies.xml",false);
 xmlhttp.send();
 xmlDoc=xmlhttp.responseXML; 
                         
