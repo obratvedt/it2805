441ca392368b4aede108be45ac16859221d6bb72
obratvedt <oyvindbratvedt@hotmail.com>
2014-11-20 12:49:17 +0100

    test outsourcing of getmovies

---
+++ webcode/movies/movies.js
@@ -0,0 +1,15 @@
+function getMovies(){
+if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+}
+                          
+else {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.send();
+xmlDoc=xmlhttp.responseXML; 
+                        
+var x=xmlDoc.getElementsByTagName("movie");
+return x;
+}
+
