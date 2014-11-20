d8a099bb5ec1038cc6c386c8973fecd00ddc7828
hermanmk <lex5050@live.no>
2014-11-20 16:33:29 +0100

    Perfected movies/index.html. edited movies.js

--- webcode/movies/movies.js
+++ webcode/movies/movies.js
@@ -1,16 +1,34 @@
 function getMovies(){
-if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-}
-                          
-else {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
- xmlhttp.open("GET","movies.xml",false);
-xmlhttp.send();
-xmlDoc=xmlhttp.responseXML; 
+    if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    } else {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET","movies.xml",false);
+    xmlhttp.send();
+    xmlDoc=xmlhttp.responseXML; 
                         
-var x=xmlDoc.getElementsByTagName("movie");
-return x;
+    var x=xmlDoc.getElementsByTagName("movie");
+    return x;
+}
+
+var idForm;
+
+function initMovie() {
+    idForm = document.idContainer.id;
+    displayMovie();
+}
+
+function displayMovie() {
+    var locate = window.location;
+    idForm.value = locate;
+
+    var text = idForm.value;
+    console.log(splitLink(text));
+}
+
+function splitLink(str) {
+    theleft = str.indexOf("=") + 1;
+    return(str.substring(theleft));
 }
 
