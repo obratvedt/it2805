0d4fc371539dfa39977096bfa0a54680d895f788
hermanmk <lex5050@live.no>
2014-11-21 02:25:35 +0100

    Added display for all info and styled movie.html

--- webcode/movies/movies.js
+++ webcode/movies/movies.js
@@ -20,15 +20,18 @@ function initMovie() {
 }
 
 function displayMovie() {
-    var locate = window.location;
-    idForm.value = locate;
-
-    var text = idForm.value;
-    console.log(splitLink(text));
-    
-    var i = splitLink(text);
     
     var movies = getMovies();
+    
+    var locate = window.location;
+    idForm.value = locate;
+    var text = idForm.value;    
+    var i = splitLink(text);
+    if(!(i % 1 === 0) || i >= movies.length){ // no, it's not an Integer
+        window.location.assign("index.html");
+        return;
+    }
+    
     var image = movies[i].getElementsByTagName("image")[0].childNodes[0].nodeValue;
     var title = movies[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
     var year = movies[i].getElementsByTagName("year")[0].childNodes[0].nodeValue;
@@ -40,7 +43,14 @@ function displayMovie() {
     var views = movies[i].getElementsByTagName("views")[0].childNodes[0].nodeValue;
     
     var infoE = document.getElementById("movie_info");
-    infoE.innerHTML = "<h2>"+title+"</h2><img src='"+image+"'/>";
+    var titleRow = "<tr><td colspan='2' class='no_indent'><h1>"+title+" ("+year+")</h1></td></tr>";
+    var imageCol = "<tr><td rowspan='5' class='no_indent'><img src='"+image+"'/></td>";
+    var textCol = "<td>Rating: <span class='bigText'>"+rating+"/6</span></td></tr><tr><td>Views: <span class='bigText'>"+views+"</span></td></tr>";
+    var textCol2 = "<tr><td>Director: <span class='bigText'>"+director+"</span></td></tr><tr><td>Category: <span class='bigText'>"+category+"</span></td></tr>";
+    var textCol3 = "<tr><td>Plot:<br><span class='bigText'>"+description+"</span></td></tr><tr><td colspan='2'><a id='rent_link' href='rent.html?movieId="+i+"'><div id='rent_button'><i class='fa fa-shopping-cart'></i> Rent '"+title+"'</div></a></td></tr>";
+    infoE.innerHTML = "<table id='movie_table'>"+titleRow+imageCol+textCol+textCol2+textCol3+"</table>";
+    
+    document.getElementById("movie_trailer").src = "http://www.youtube.com/embed/" + trailer;
                         
 }
 
